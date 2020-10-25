502d03d5cd2e7f7ad005d4440254df2ee7c8abc2
yep-dev <yep.dev7@gmail.com>
2020-10-25 17:30:51 +0100

    better square wave generation

--- backend/app/app/api/endpoints/wave.py
+++ backend/app/app/api/endpoints/wave.py
@@ -10,19 +10,28 @@ from starlette.requests import Request
 
 router = APIRouter()
 
+RESOLUTION = 20
+
 
 class WaveSpec(BaseModel):
     type: str
+    duration: float
 
 
-def sine():
-    linespace = np.linspace(-np.pi, np.pi, 51)
+def sine(duration):
+    linespace = np.linspace(-np.pi, np.pi, int(RESOLUTION * duration) + 1)
     return np.sin(linespace)
 
 
-def square():
-    linespace = np.linspace(0, 10, 51, endpoint=True)
-    return signal.square(500 * linespace)
+def square(duration):
+    def process_point(point, points_number):
+        if points_number / 4 <= point < points_number / 4 * 3:
+            return 1
+        return -1
+
+    linespace = np.linspace(0, int(RESOLUTION * duration), int(RESOLUTION * duration),
+                            endpoint=True)
+    return np.array([process_point(i, int(RESOLUTION * duration)) for i in linespace])
 
 
 def reverse_gradient(gradient, linespace):
@@ -38,7 +47,7 @@ def reverse_gradient(gradient, linespace):
 
 async def build_wave(spec):
     types = {"sine": sine, "square": square}
-    original_positions = types.get(spec.type, lambda: None)()
+    original_positions = types.get(spec.type, lambda: None)(spec.duration)
     movements = np.gradient(original_positions)
     # process movements here
 
@@ -53,12 +62,14 @@ async def build_wave(spec):
 
 @router.post('/get')
 async def wave(request: Request, spec: WaveSpec):
+    # todo: send flag if wave doesn't start at 0 to block running it
     data = await build_wave(spec)
     return data
 
 
 @router.post("/run")
 async def wave(request: Request, spec: WaveSpec):
+    # todo: ensure that each wave ran starts at 0 position
     data = await build_wave(spec)
 
     redis = request.app.extra["redis"]
