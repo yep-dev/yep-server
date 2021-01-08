07b6dd06d62af4ed3016a3f642d2a5c07ef9bfd0
yep-dev <yep.dev7@gmail.com>
2021-01-08 18:04:33 +0100

    cleanup

--- backend/app/app/api/endpoints/curve.py
+++ backend/app/app/api/endpoints/curve.py
@@ -4,10 +4,9 @@ from fastapi import APIRouter
 from scipy.interpolate import make_interp_spline
 
 import numpy as np
+from pydantic import BaseModel
 
 router = APIRouter()
-
-from pydantic import BaseModel
 
 
 class Data(BaseModel):
@@ -16,8 +15,8 @@ class Data(BaseModel):
 
 @router.post("/")
 def post_settings_curve(
-    *,
-    data: Data,
+        *,
+        data: Data,
 ) -> Any:
     print(data)
     x = [0, 100, 200, 300, 400, 500, 600, 700, 800, 900, 1000]
--- backend/app/app/schemas/__init__.py
+++ backend/app/app/schemas/__init__.py
@@ -1,8 +1,4 @@
-from .item import Item, ItemCreate, ItemInDB, ItemUpdate
-from .msg import Msg
 from .settings import (
     MachineThrustSettingsEdit,
     MachineThrustSettingsDisplay,
 )
-from .token import Token, TokenPayload
-from .user import User, UserCreate, UserInDB, UserUpdate
